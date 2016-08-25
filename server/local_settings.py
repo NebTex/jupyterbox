cc8791925be2591855dd7fcc25945c3ab97ebd4b
Cristian Lozano <criscalovis@gmail.com>
2016-08-25 13:35:59 -0500

    initial

---
+++ server/local_settings.py
@@ -0,0 +1,8 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'cristian',
+        'USER': 'cristian',
+        'PASSWORD': 'cristian',
+    }
+}
